f99f9b3136f9f84ec91d2185bd51e0050dec3ad5
Jaehoon Choi <plaintext@andromedarabbit.net>
2016-12-27 18:58:30 +0900

    `logrotate.conf` 설정을 환경변수로 제어할 수 있다

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,8 @@ if [ "${UP_AND_RUN}" = "true" ]; then
   truncate -s 0 /var/lib/docker/containers/*/*.log
 fi
 
+if [ -n "${BASE64_ENCODED_LOGROTATE_CONF}" ]; then
+  echo "${BASE64_ENCODED_LOGROTATE_CONF}" | base64 -d > /etc/logrotate.conf
+fi
+
 exec "$@"
